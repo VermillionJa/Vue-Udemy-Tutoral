19e5ce595836d81bde06ca75465b078ed7aaf34c
Jake Vermillion <jacob.a.vermillion@gmail.com>
2018-10-21 11:36:42 -0600

    Example of two-way property binding

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
 new Vue({
     el: "#app",
     data: {
-        counter: 0,
-        x: 0,
-        y: 0
-    },
-    methods: {
-        incrementCounter(step, e) {
-            this.counter += step;
-        },
-        updateCoordinates(e) {
-            this.x = e.clientX;
-            this.y = e.clientY;
-        },
-        alertMe() {
-            console.log("Enter pressed");
-        }
+        name: "Jake"
     }
 });
