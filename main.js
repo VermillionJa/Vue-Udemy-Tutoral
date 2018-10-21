e237fec097beed603376988b466481df06c97e41
Jake Vermillion <jacob.a.vermillion@gmail.com>
2018-10-21 12:17:03 -0600

    Example of using the shorthand for v-bind and v-on

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
 new Vue({
     el: "#app",
     data: {
-        counter: 0,
-        secondCounter: 0
+        link: "http://google.com"
     },
-    computed: {
-        result() {
-            return this.counter > 5 ? "X > 5" : "X <= 5";
-        }
-    },
-    watch: {
-        counter: function(value) {
-            var vm = this;
-            setTimeout(() => {
-                vm.counter = 0;
-            }, 2000);
+    methods: {
+        changeLink() {
+            this.link = "http://www.vuejs.org";
         }
     }
 });
