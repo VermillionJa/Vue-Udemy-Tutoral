b319f91478d4c422f176bdc13f16d5f04b796799
Jake Vermillion <jacob.a.vermillion@gmail.com>
2018-10-21 15:51:30 -0600

    Example of using bound styles and style objects

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 new Vue({
     el: "#app",
     data: {
-        attachRed: false,
-        color: 'green'
+        color: 'gray',
+        width: 100
     },
     computed: {
-        divClasses() {
-            return { 
-                red: this.attachRed, 
-                blue: !this.attachRed
+        myStyle() {
+            return{
+                backgroundColor: this.color,
+                width: this.width + "px"
             };
         }
     }
