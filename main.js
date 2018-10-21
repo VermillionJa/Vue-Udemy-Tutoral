0b0d3301debb11409b9f75a12f21d113ceb803f4
Jake Vermillion <jacob.a.vermillion@gmail.com>
2018-10-21 12:07:39 -0600

    Example of Watching Properties

--- main.js
+++ main.js
@@ -8,5 +8,13 @@ new Vue({
         result() {
             return this.counter > 5 ? "X > 5" : "X <= 5";
         }
+    },
+    watch: {
+        counter: function(value) {
+            var vm = this;
+            setTimeout(() => {
+                vm.counter = 0;
+            }, 2000);
+        }
     }
 });
